0879f1542effd716c6fb46ba53001b77454ddd93
Grossevich Lev <levgross@gmail.com>
2023-01-06 12:44:14 +0400

    1. Техническое задание - проект доставка еды "Быстро и в точку" [#504899 #377902]

--- dish/src/main/java/ru/job4j/dish/service/DishService.java
+++ dish/src/main/java/ru/job4j/dish/service/DishService.java
@@ -1,12 +1,10 @@
 package ru.job4j.dish.service;
 
-import org.springframework.stereotype.Service;
 import ru.job4j.domain.model.Dish;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface DishService {
     Optional<Dish> addDish(Dish dish);
     Optional<Dish> editDish(Dish dish);
